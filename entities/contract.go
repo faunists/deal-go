b53e65608955a00e3dfb641e6ba76b992b17d6c3
Pablo Aguilar <pablo.aguilar@outlook.com.br>
2021-11-28 23:34:26 -0300

    Adds YAML support

--- entities/contract.go
+++ entities/contract.go
@@ -4,8 +4,8 @@ import "fmt"
 
 // Contract represents the root of everything that will be generated
 type Contract struct {
-	Name     string             `json:"name"`
-	Services map[string]Service `json:"services"`
+	Name     string             `json:"name" yaml:"name"`
+	Services map[string]Service `json:"services" yaml:"services"`
 }
 
 // Service is a named type of a map[string]Method
@@ -16,29 +16,29 @@ type Service map[string]Method
 //   - Success
 //   - Failure
 type Method struct {
-	SuccessCases []SuccessCase `json:"successCases"`
-	FailureCases []FailureCase `json:"failureCases"`
+	SuccessCases []SuccessCase `json:"successCases" yaml:"successCases"`
+	FailureCases []FailureCase `json:"failureCases" yaml:"failureCases"`
 }
 
 // SuccessCase handles the information about the request and response of a method
 type SuccessCase struct {
-	Description string      `json:"description"`
-	Request     interface{} `json:"request"`
-	Response    interface{} `json:"response"`
+	Description string      `json:"description" yaml:"description"`
+	Request     interface{} `json:"request" yaml:"request"`
+	Response    interface{} `json:"response" yaml:"response"`
 }
 
 // FailureCase handles the information about the request and the error that should be returned
 // for a given request
 type FailureCase struct {
-	Description string      `json:"description"`
-	Request     interface{} `json:"request"`
-	Error       GRPCError   `json:"error"`
+	Description string      `json:"description" yaml:"description"`
+	Request     interface{} `json:"request" yaml:"request"`
+	Error       GRPCError   `json:"error" yaml:"error"`
 }
 
 // GRPCError handles the information about the error code and the string message of a GRPC error
 type GRPCError struct {
-	ErrorCode string `json:"errorCode"`
-	Message   string `json:"message"`
+	ErrorCode string `json:"errorCode" yaml:"errorCode"`
+	Message   string `json:"message" yaml:"message"`
 }
 
 func (e GRPCError) String() string {
--- processors/contract.go
+++ processors/contract.go
@@ -2,20 +2,39 @@ package processors
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 
 	"github.com/faunists/deal-go/entities"
 )
 
 // ReadContractFile reads a JSON File and try to parse it to a entities.Contract object
 func ReadContractFile(filePath string) (entities.Contract, error) {
-	jsonData, err := ioutil.ReadFile(filePath)
+	splitFilePath := strings.Split(filePath, ".")
+	extension := splitFilePath[len(splitFilePath)-1]
+
+	var unmarshaler func([]byte, interface{}) error
+	switch extension {
+	case "json":
+		unmarshaler = json.Unmarshal
+	case "yaml", "yml":
+		unmarshaler = yaml.Unmarshal
+	default:
+		return entities.Contract{}, errors.New(
+			"invalid contract extension, supported formats are json and yaml",
+		)
+	}
+
+	fileData, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return entities.Contract{}, err
 	}
 
 	rawContract := entities.Contract{}
-	if err = json.Unmarshal(jsonData, &rawContract); err != nil {
+	if err = unmarshaler(fileData, &rawContract); err != nil {
 		return entities.Contract{}, err
 	}
 
